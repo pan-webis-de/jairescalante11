0a93e94b7777c88b133e95fbf09246735478098f
Michael Traeger <git@m.traeger-mail.de>
2015-10-06 15:58:36 +0200

    histograms sequential into one hist

--- local-hist_impl/src/de/webis/nizza/localhistograms/aggregatedHistogram/BagOfLocalHistograms.java
+++ local-hist_impl/src/de/webis/nizza/localhistograms/aggregatedHistogram/BagOfLocalHistograms.java
@@ -1,14 +1,28 @@
 package de.webis.nizza.localhistograms.aggregatedHistogram;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class BagOfLocalHistograms implements AggregatedHistogram {
 
+	private int singleHistogramSize;
+
 	@Override
 	public List<Double> generateAggregatedHistogram(List<String> vocabulary,
 			List<List<Double>> localHistograms) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Double> bolh = new LinkedList<>();
+
+		for (List<Double> singleHistogram : localHistograms) {
+			bolh.addAll(singleHistogram);
+		}
+
+		singleHistogramSize = localHistograms.get(0).size();
+
+		return bolh;
+	}
+
+	public int getSingleHistogramSize() {
+		return singleHistogramSize;
 	}
 
 }
