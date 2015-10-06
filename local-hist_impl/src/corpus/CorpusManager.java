a7a485fe935b903b5427b0547c22ba1ffca858f5
Michael Traeger <git@m.traeger-mail.de>
2015-10-06 02:41:31 +0200

    commented encoding because of Nullpointer in pan11large

--- local-hist_impl/src/corpus/CorpusManager.java
+++ local-hist_impl/src/corpus/CorpusManager.java
@@ -27,123 +27,117 @@ public class CorpusManager implements ICorpusManager {
 	private List<TextInstance> knownTexts;
 	private List<TextInstance> unknownTexts;
 	private HashMap<String, String> authorTextMapping;
-	
+
 	private Iterator<TextInstance> knownTextIterator;
 	private Iterator<TextInstance> unknownTextIterator;
 
 	public CorpusManager(String location) throws IOException {
 		corpusLocation = new File(location).toPath();
 		File metaFile = new File(corpusLocation.toFile(), metaFileName);
-		File groundTruthFile = new File(corpusLocation.toFile(), groundTruthFileName);
+		File groundTruthFile = new File(corpusLocation.toFile(),
+				groundTruthFileName);
 
 		if (!metaFile.exists()) {
-			throw new IOException("Could not find meta file '" + metaFile.toString() + "'");
+			throw new IOException("Could not find meta file '"
+					+ metaFile.toString() + "'");
 		}
 
 		if (!groundTruthFile.exists()) {
-			throw new IOException("Could not find groundTruth file '" + groundTruthFile.toString() + "'");
+			throw new IOException("Could not find groundTruth file '"
+					+ groundTruthFile.toString() + "'");
 		}
 
 		JsonObject metadata = null;
 		JsonObject groundData = null;
-		
+
 		try (InputStream metaInputStream = new FileInputStream(metaFile);
 				JsonReader metaReader = Json.createReader(metaInputStream);
-				InputStream groundInputStream = new FileInputStream(groundTruthFile);
+				InputStream groundInputStream = new FileInputStream(
+						groundTruthFile);
 				JsonReader groundReader = Json.createReader(groundInputStream);) {
 
 			metadata = metaReader.readObject();
 			groundData = groundReader.readObject();
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			throw new IOException("Failed to read JSON: " + e.getMessage());
 		}
-		
-		encoding = metadata.getString("encoding");
+
+		// encoding = metadata.getString("encoding");
 		unknownFolder = metadata.getString("folder");
 		language = metadata.getString("language");
-		
+
 		authors = new ArrayList<String>();
-		for(JsonObject author : metadata.getJsonArray("candidate-authors").getValuesAs(JsonObject.class))
-		{
+		for (JsonObject author : metadata.getJsonArray("candidate-authors")
+				.getValuesAs(JsonObject.class)) {
 			authors.add(author.getString("author-name"));
 		}
-		
+
 		unknownTexts = new ArrayList<TextInstance>();
 		discoverUnknownTexts();
 		unknownTextIterator = unknownTexts.iterator();
-		
+
 		knownTexts = new ArrayList<TextInstance>();
 		discoverKnownTexts();
 		knownTextIterator = knownTexts.iterator();
-		
+
 		authorTextMapping = new HashMap<String, String>();
-		for (JsonObject truth : groundData.getJsonArray("ground-truth").getValuesAs(JsonObject.class))
-		{
-			authorTextMapping.put(truth.getString("unknown-text"), truth.getString("true-author"));
+		for (JsonObject truth : groundData.getJsonArray("ground-truth")
+				.getValuesAs(JsonObject.class)) {
+			authorTextMapping.put(truth.getString("unknown-text"),
+					truth.getString("true-author"));
 		}
 	}
 
 	private void discoverKnownTexts() {
-		assert(authors.size() > 0);
-		
-		for (String author : authors)
-		{
+		assert (authors.size() > 0);
+
+		for (String author : authors) {
 			File authorFolder = new File(corpusLocation.toFile(), author);
-			if ((!authorFolder.exists()) || (!authorFolder.isDirectory()))
-			{
-				System.err.println("Could not open folder " + authorFolder + ", skipping.");
+			if ((!authorFolder.exists()) || (!authorFolder.isDirectory())) {
+				System.err.println("Could not open folder " + authorFolder
+						+ ", skipping.");
 				continue;
 			}
-			
+
 			List<Path> texts = Utilities.getDirectoryContents(authorFolder);
-			for (Path text : texts)
-			{
-				if (text.toFile().exists())
-				{
-					TextInstance instance = new TextInstance(author, text.toFile());
+			for (Path text : texts) {
+				if (text.toFile().exists()) {
+					TextInstance instance = new TextInstance(author,
+							text.toFile());
 					knownTexts.add(instance);
-				}
-				else
-				{
-					System.err.println("Could not locate file " + text.toString() + ", skipping.");
+				} else {
+					System.err.println("Could not locate file "
+							+ text.toString() + ", skipping.");
 					continue;
 				}
 			}
 		}
 	}
-	
-	private void discoverUnknownTexts()
-	{
+
+	private void discoverUnknownTexts() {
 		File unknown = new File(corpusLocation.toFile(), unknownFolder);
 		List<Path> texts = Utilities.getDirectoryContents(unknown);
-		for (Path unknownText : texts)
-		{
-			TextInstance instance = new TextInstance(unknownText.toFile().getName(), unknownText.toFile());
+		for (Path unknownText : texts) {
+			TextInstance instance = new TextInstance(unknownText.toFile()
+					.getName(), unknownText.toFile());
 			unknownTexts.add(instance);
 		}
 	}
-	
+
 	@Override
 	public TextInstance getNextText() {
-		if (knownTextIterator.hasNext())
-		{
+		if (knownTextIterator.hasNext()) {
 			return knownTextIterator.next();
-		}
-		else
-		{
+		} else {
 			return null;
 		}
 	}
 
 	@Override
 	public boolean validateUnknownAttribution(File text, String author) {
-		if (authorTextMapping.get(text) == author)
-		{
+		if (authorTextMapping.get(text) == author) {
 			return true;
-		}
-		else
-		{
+		} else {
 			return false;
 		}
 	}
@@ -172,21 +166,18 @@ public class CorpusManager implements ICorpusManager {
 
 	@Override
 	public TextInstance getUnknownText() {
-		if (unknownTextIterator.hasNext())
-		{
+		if (unknownTextIterator.hasNext()) {
 			return unknownTextIterator.next();
-		}
-		else
-		{
+		} else {
 			return null;
-		} 
+		}
 	}
 
 	@Override
 	public int getUnknownTextCount() {
 		return unknownTexts.size();
 	}
-	
+
 	public HashMap<String, String> getAuthorTextMapping() {
 		return authorTextMapping;
 	}
