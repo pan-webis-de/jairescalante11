aae8c0f94dc0f83d4d14af3946e7a33c2330918f
Michael Träger <git@m.traeger-mail.de>
2015-10-29 00:32:21 +0100

    added input arg code

--- local-hist_impl/src/de/webis/nizza/localhistograms/Main.java
+++ local-hist_impl/src/de/webis/nizza/localhistograms/Main.java
@@ -12,16 +12,27 @@ public class Main {
 		if (args == null || args.length == 0) {
 			System.err.println("Please provide correct arguments.");
 		} else {
-			// String inPath = "";
-			// if (args[0].equals("-i")) {
-			// inPath = args[1];
-			// }
+
+			// TODO validate input args
+			String inPath = "";
+			if (args[0].equals("-i")) {
+				inPath = args[1];
+			} else {
+				inPath = "../corpora/C10/";
+			}
+
+			String outPath = "";
+			if (args[2].equals("-o")) {
+				outPath = args[3];
+			} else {
+				outPath = inPath + "output.json";
+			}
 
 			try {
-				String inPath = "../corpora/C10/";
+
 				List<EnrichedSvmResult> results = new LocalHistogramAnalyzer(
 						inPath).analyze();
-				new ResultWriter(inPath + "output.json").writeJsonFile(results);
+				new ResultWriter(outPath).writeJsonFile(results);
 			} catch (IOException up) {
 				System.err.println("Fail. " + up.getMessage());
 				throw up;
